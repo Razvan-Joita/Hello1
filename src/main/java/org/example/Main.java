58b7c6b7b228b76d1ad4271f2e611c7cec176efe
Joita Razvan-Gabriel <joitarazvan@yahoo.com>
2023-11-10 23:20:06 +0200

    FirstCommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x=10;
+        System.out.println(x);
     }
 }
